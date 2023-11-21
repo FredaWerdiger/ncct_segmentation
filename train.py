14e68ea61601ee6b3bb7a51db362c11c2d88663a
FredaWerdiger <fwerdiger@gmail.com>
2023-11-21 13:21:15 +1100

    testing

--- train.py
+++ train.py
@@ -190,7 +190,7 @@ def main():
     val_files = make_dict(val_ids)[:4]
     test_files = make_dict(test_ids)[:4]
 
-    max_epochs = 2
+    max_epochs = 4
     image_size = [128]
     batch_size = 2
     val_interval = 2
